a78e98437e8c75ff1f941f5c6548695fe71ade36
Austin <babelontrip@gmail.com>
2024-11-12 11:27:53 -0600

    grid layout at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import { AccordionMenu } from '@/components/menu/accordion-menu'
 
 export default function Home() {
   return (
-    <section className="flex flex-col h-screen w-full relative">
-      <div className="mt-2 h-max absolute top-2 right-2">
+    <section className="grid  grid-rows-[50px_1fr_100px] h-screen w-full relative ">
+      <div className="mt-2 h-max justify-self-end">
         <SwitchTheme />
       </div>
       <div className="px-5 mt-10 md:px-20 md:mt-10">
         <AccordionMenu />
+      </div>
+
+      <div className="justify-self-center self-end mb-3">
+        <span className="font-bold text-foreground-800 uppercase text-5xl text-center ">
+          holbox
+        </span>
       </div>
     </section>
   )
